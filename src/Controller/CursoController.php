04c091a79a14261f657f0aaf4907d3d7369f55db
Víctor M Vaquero <vvm1002@alu.ubu.es>
2024-10-07 18:04:23 +0200

    intentanto instalar PHPStan

--- src/Controller/CursoController.php
+++ src/Controller/CursoController.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CursoController extends AbstractController
+{
+    #[Route('/curso', name: 'curso')]
+    public function index(): Response
+    {
+        return new Response('Página de Cursos');
+    }
+}
